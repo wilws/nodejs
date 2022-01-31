adea5c66eb9ddc2fe6c9660163ae767448839b48
wilws <wilson.ws.pro@gmail.com>
2022-01-31 22:22:13 +0800

    S6_78-87_Pug_template

--- routes/admin.js
+++ routes/admin.js
@@ -11,6 +11,7 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
+  res.render('add-product',{pageTitle:"Add Product", path: '/admin/add-product'})
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,10 @@ const router = express.Router();
 const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-  console.log('adminData.product')
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // console.log('adminData.product')
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {prods: products, pageTitle:"Shop", path: '/'});
 });
 
 module.exports = router;
