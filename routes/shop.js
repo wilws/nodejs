6406d86508755886f5be2e39d9b1e4d4b8f1bf7c
wilws <wilson.ws.pro@gmail.com>
2022-01-31 19:50:30 +0800

    template engine

--- routes/shop.js
+++ routes/shop.js
@@ -5,8 +5,10 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
+  console.log('adminData.product')
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
